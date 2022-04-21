672f70a74ea370319a1d6f3b15d2919043e80e02
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-22 03:56:56 +0600

    add user add user delete and user read with ui display

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Running API')
+});
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.drus2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db('geniusCar').collection('services');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service)
+        })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        // POST
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.send(result);
+        })
+
+        // DELETE
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log('Code is ok');
+})
