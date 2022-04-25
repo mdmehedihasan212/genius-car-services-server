515c201da3b00663856c59454f04264c46ec656f
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-26 00:43:04 +0600

    add extra check api

--- __steps_deploy.js
+++ __steps_deploy.js
@@ -1,18 +1,27 @@
 /**
- * ----------------------
+ * -----------------------
  * ONE TIME your computer
- * ----------------------
+ * -----------------------
  * Create heroku account
  * Verify your email account
  * Select your language and proceed
  * Go their documentation
  * Install heroku CLI
  * heroku login
- * ----------------
+ * -----------------
  * FOR EACH PROJECT
- * ----------------
+ * -----------------
  * First of all check your server cmd
  * heroku create
  * git add . git commit -m"" git push
- * heroku git push main
+ * git push heroku main
+ * Go heroku dashboard > Current Project > Settings > Reveal Config Vars
+ * Copy paste .env file for config key and value
+ * Make sure your whitelisted ip address to access mongodb
+ * --------------
+ * UPDATE SERVER
+ * --------------
+ * Make change
+ * git add . git commit -m"" git push
+ * git push heroku main
  */
--- index.js
+++ index.js
@@ -30,6 +30,10 @@ app.get('/', (req, res) => {
     res.send('Genius Car Services')
 });
 
+app.get('/check', (req, res) => {
+    res.send('Heroku is Ok')
+})
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.drus2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
